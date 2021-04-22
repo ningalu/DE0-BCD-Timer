e51de47228080b9f2c65eba264c4ad4e79e8cd0d
ningalu <thomasholster2012@gmail.com>
2021-04-22 13:13:14 +1200

    full synthesizable solution

--- Timer Testbench.vhd
+++ Timer Testbench.vhd
@@ -10,24 +10,24 @@ Architecture Test OF TimerBench IS
     SIGNAL t_Data_In: STD_LOGIC_VECTOR(9 DOWNTO 0);
     SIGNAL t_tClk, t_tStart: STD_LOGIC;
     SIGNAL t_Time_Out: STD_LOGIC;
-    SIGNAL t_Count, t_Count1: STD_LOGIC_VECTOR(9 DOWNTO 0);
+    --SIGNAL t_Count, t_Count1: STD_LOGIC_VECTOR(9 DOWNTO 0);
     SIGNAL t_mOut, t_suOut, t_slOut: STD_LOGIC_VECTOR(7 DOWNTO 0);
     COMPONENT Timer IS
         PORT (
             Data_In: IN STD_LOGIC_VECTOR(9 DOWNTO 0);
             tClk, tStart: IN STD_LOGIC;
             Time_Out: OUT STD_LOGIC;
-            Count, Count1: OUT STD_LOGIC_VECTOR(9 DOWNTO 0); -- temporary
+            --Count, Count1: OUT STD_LOGIC_VECTOR(9 DOWNTO 0); -- temporary
             mOut, suOut, slOut: OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
         );
     END COMPONENT;
 BEGIN
-    DUT: Timer Port Map (Data_In => t_Data_In, tClk => t_tClk, tStart => t_tStart, Time_Out => t_Time_Out, Count => t_Count, Count1 => t_Count1, mOut => t_mOut, suOut => t_suOut, slOut => t_slOut);
+    DUT: Timer Port Map (Data_In => t_Data_In, tClk => t_tClk, tStart => t_tStart, Time_Out => t_Time_Out, mOut => t_mOut, suOut => t_suOut, slOut => t_slOut);
 
     init: PROCESS
     BEGIN
         t_tStart <= '1';
-        t_Data_In <= "0000000100";
+        t_Data_In <= "1000000100";
         WAIT;
     END PROCESS init;
 
